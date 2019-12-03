e6b1479366092fc5a7ef2178310d03413ba0d379
Alexander Girke <alexander.girke@mercateo.com>
2019-12-03 10:23:06 +0100

    WIP

--- hyperledger-composer/aktivist-network/lib/logic.js
+++ hyperledger-composer/aktivist-network/lib/logic.js
@@ -1,10 +1,38 @@
-/**
- * Track the trade of a commodity from one trader to another
- * @param {org.uni.leipzig.aktivist.Trade} trade - the trade to be processed
- * @transaction
- */
-async function tradeCommodity(trade) {
-    trade.commodity.owner = trade.newOwner;
-    let assetRegistry = await getAssetRegistry('org.uni.leipzig.aktivist.Commodity');
-    await assetRegistry.update(trade.commodity);
+async function aktivitaetErfuellt(event) {
+
+    /**
+     * Update konto
+     */
+    let konto = event.benutzer.konto;
+
+    konto.ehrentaler += event.ehrentaler
+    konto.ehrenpunkte += event.ehrenpunkte
+
+    let kontoAssetRegistry = await getAssetRegistry('org.uni.leipzig.aktivist.Konto');
+
+    await kontoAssetRegistry.update(konto);
+
+    /**
+     * Aktivitaet
+     */
+    let aktivitaet = event.aktivitaet
+    
+    let benutzerAktivitaeten = event.benutzer.aktivitaeten
+    benutzerAktivitaeten += aktivitaet
+
+    let aktivitaetenAssetRegistry = await getAssetRegistry('org.uni.leipzig.aktivist.Aktivitaet');
+
+    await aktivitaetenAssetRegistry.update(benutzerAktivitaeten);
 }
+
+// async function tradeCommodity(trade) {
+//     trade.commodity.owner = trade.newOwner;
+//     let assetRegistry = await getAssetRegistry('org.uni.leipzig.aktivist.Commodity');
+//     await assetRegistry.update(trade.commodity);
+// }
+
+// async function tradeCommodity(trade) {
+//     trade.commodity.owner = trade.newOwner;
+//     let assetRegistry = await getAssetRegistry('org.uni.leipzig.aktivist.Commodity');
+//     await assetRegistry.update(trade.commodity);
+// }
