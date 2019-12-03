a6ac99d4344571127c61b2aac3185334c65f7271
Anja <anja.ohlhaeuser@googlemail.com>
2019-12-03 10:41:50 +0100

    edit some shit

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -5,13 +5,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import {BrowserRouter, Route, Redirect, useParams, Switch} from "react-router-dom";
+import Finder from "./Finder";
+import {NavLink} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function Copyright() {
   return (
@@ -92,6 +96,7 @@ export default function Login() {
             variant="contained"
             color="primary"
             className={classes.submit}
+            component={Link} to="/finder"
           >
             Sign In
           </Button>
@@ -114,4 +119,8 @@ export default function Login() {
       </Box>
     </Container>
   );
+}
+
+function handleOnClick() {
+
 }
